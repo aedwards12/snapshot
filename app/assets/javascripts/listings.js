08961cad9c222be5719dd6cf33df5533ed22fe88
Anthony Edwards Jr <credential.helper>
2014-09-14 18:34:51 -0400

    added grid size

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
       function initialize() {
         var mapOptions = {
           center: { lat: 40.7056308, lng: -73.9780035},
-          zoom: 11
+          zoom: 12
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
