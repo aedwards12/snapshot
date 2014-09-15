0b220debdc7a1401f89e91fd2ebdc0b56a54bf11
Anthony Edwards Jr <credential.helper>
2014-09-14 20:47:27 -0400

    on mouseover the street address is displayed

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -1,55 +1,52 @@
-document.addEventListener('DOMContentLoaded', function(){
-
-      
-        var mapOptions = {
-          center: { lat: 40.7056308, lng: -73.9780035},
-          zoom: 12
-        };
-      
-      
-       map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
-      // google.maps.event.addDomListener(window, 'load', initialize);
-		  var pos = new google.maps.LatLng(40.758895, -73.985131)
-			
-			var marker = new google.maps.Marker({
-			position: pos,
-	    map: map,
-	    title:"Hello World!"
-
-
-	});
-
-			gatherListings();
+document.addEventListener('DOMContentLoaded', function(){   
+  
+  var mapOptions = {
+    center: { lat: 40.7056308, lng: -73.9780035},
+    zoom: 12
+  };
+     
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+	gatherListings(map);
 
 });
 
-function gatherListings(){
-	// console.log("loading");
-	var listings = document.querySelectorAll(".listing")
-	// console.log(listings.length);
+function gatherListings(map){
 
+	var listings = document.querySelectorAll(".listing")
 	for(var i = 0;  i < listings.length;i++){
 		var lat =listings[i].querySelector("#lat").innerHTML;
 		var lng = listings[i].querySelector("#long").innerHTML;
-
+		var address = listings[i].querySelector("#address").innerHTML;
 		if (lat != null){ 
-		  appendListing(lat, lng);
+		  appendListing(lat, lng, address, map);
+		  // addListener(address);
 	  }
-
 	}
 }
 
-function appendListing(lat, lng){
-		console.log(lat);
+function appendListing(lat, lng, address, map){
 
-	// var map = google.maps.Map(document.getElementById('map-canvas'));
-	var pos = new google.maps.LatLng(lat, lng)
-			
-			var marker = new google.maps.Marker({
-			position: pos,
-	    title:"Hello World!"
+	var pos = new google.maps.LatLng(lat, lng)	
+	var marker = new google.maps.Marker({
+	  position: pos,
+    title: address
 	});
-			marker.setMap(map);
+
+	marker.setMap(map);
+	_addListener(marker, address);
+}
+
+function _addListener(marker, address){
+	console.log(address)
+	var infowindow = new google.maps.InfoWindow({
+		content: address
+	});
+
+	google.maps.event.addListener(marker, 'mouseover', function(){
+		infowindow.open(marker.get('map'), marker);
+	});
+	google.maps.event.addListener(marker, 'mouseout', function(){
+		infowindow.close();
+	});
 }
