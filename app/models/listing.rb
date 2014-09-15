d128bcc776beb10598b7076c15a5f82dda0b0ef0
Farheen Malik <farheen.m.malik@gmail.com>
2014-09-14 20:38:24 -0400

    Change multisearch to pg_search_scope and filter by address abd assicated tag

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,8 @@
 class Listing < ActiveRecord::Base
 	include PgSearch
-  multisearchable :against => [:address]
+  pg_search_scope :listing_search, :against => [:address], :associated_against => {
+    :tags => [:name]
+  }
   geocoded_by :address
   after_validation :geocode
 
